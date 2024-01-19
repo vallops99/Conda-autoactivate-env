0303f44936184bec8b6382edf83ba30098d6ab81
Ryan Horrisberger <donotgetemail@gmail.com>
2024-01-19 15:21:03 -0600

    Add conda initialization check
    Now it will only have an effect if conda is active (user has previously run 'conda init'). Thus it may be added to the system-wide functions in /etc/bashrc || /etc/zshrc and it will add functionality for all users, but only users who've initialized conda.

--- addToShellProfile.sh
+++ addToShellProfile.sh
@@ -1,11 +1,13 @@
-export CONDACONFIGDIR=""
-cd() { builtin cd "$@" && 
-if [ -f $PWD/.conda_config ]; then
-    export CONDACONFIGDIR=$PWD
-    conda activate $(cat .conda_config)
-elif [ "$CONDACONFIGDIR" ]; then
-    if [[ $PWD != *"$CONDACONFIGDIR"* ]]; then
-        export CONDACONFIGDIR=""
-        conda deactivate
-    fi
-fi }
+if [[ -v "$CONDA_SHLVL" ]]; then
+    export CONDACONFIGDIR=""
+    cd() { builtin cd "$@" && 
+    if [ -f $PWD/.conda_config ]; then
+        export CONDACONFIGDIR=$PWD
+        conda activate $(cat .conda_config)
+    elif [ "$CONDACONFIGDIR" ]; then
+        if [[ $PWD != *"$CONDACONFIGDIR"* ]]; then
+            export CONDACONFIGDIR=""
+            conda deactivate
+        fi
+    fi }
+fi
