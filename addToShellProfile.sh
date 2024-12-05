f3845bbe92e506e397686ebff561294226f57415
Brandon Victor <Multihuntr@hotmail.com>
2024-12-05 20:00:10 +1100

    Also trigger on bash launch

--- addToShellProfile.sh
+++ addToShellProfile.sh
@@ -1,6 +1,4 @@
-if [[ -n "$CONDA_SHLVL" ]]; then
-    export CONDACONFIGDIR=""
-    cd() { builtin cd "$@" && 
+function _z_do_conda_activate() {
     if [ -f "$PWD/.conda_config" ]; then
         export CONDACONFIGDIR=$PWD
         conda activate "$(cat .conda_config)"
@@ -9,5 +7,11 @@ if [[ -n "$CONDA_SHLVL" ]]; then
             export CONDACONFIGDIR=""
             conda deactivate
         fi
-    fi }
+    fi
+}
+if [[ -n "$CONDA_SHLVL" ]]; then
+    export CONDACONFIGDIR=""
+    cd() { builtin cd "$@" && _z_do_conda_activate
+    }
+    _z_do_conda_activate
 fi
